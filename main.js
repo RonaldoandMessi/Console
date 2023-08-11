06cc7c6574253e9f7231faba32e53b1c28172706
Spiderman <128212597+RonaldoandMessi@users.noreply.github.com>
2023-08-11 16:52:07 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name="Pranav "
+
+var last_name="Sai"
+console.log(first_name)
+console.log(last_name)
+first_name.concat(last_name)
+
+var full_name=first_name.concat(last_name)
+console.log(full_name)
+
+var square_root=Math.sqrt(16)
+console.log(square_root)
